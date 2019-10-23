1242e1fc82cf7bc2a57910cf4397a8c57868a205
Vitaly Bogdanov <vsbogd@gmail.com>
2019-10-23 18:49:01 +0300

    Add bytes32ToStr, strToBytes32 unit tests

--- src/main/java/io/singularitynet/sdk/common/Utils.java
+++ src/main/java/io/singularitynet/sdk/common/Utils.java
@@ -1,6 +1,7 @@
 package io.singularitynet.sdk.common;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static com.google.common.base.Preconditions.checkArgument;
 import java.util.concurrent.Callable;
 import java.util.Base64;
 import org.web3j.abi.datatypes.Address;
@@ -11,7 +12,12 @@ public class Utils {
         return str.getBytes(UTF_8);
     }
 
+    public static String bytesToStr(byte[] bytes) {
+        return new String(bytes, UTF_8);
+    }
+
     public static byte[] strToBytes32(String str) {
+        checkArgument(str.length() <= 32, "Passed string length exceeds 32 bytes");
         byte[] bytes32 = new byte[32];
         int i = 0;
         for (byte b : str.getBytes(UTF_8)) {
@@ -20,21 +26,18 @@ public class Utils {
         return bytes32;
     }
 
+    public static String bytes32ToStr(byte[] bytes) {
+        checkArgument(bytes.length == 32, "Passed array length is not equal to 32 bytes");
+        String full = new String(bytes, UTF_8);
+        return full.substring(0, full.indexOf(0));
+    }
+
     public static byte[] base64ToBytes(String str) {
         return Base64.getDecoder().decode(str);
     }
 
     public static String bytesToBase64(byte[] bytes) {
         return Base64.getEncoder().encodeToString(bytes);
-    }
-
-    public static String bytes32ToStr(byte[] bytes) {
-        String full = new String(bytes, UTF_8);
-        return full.substring(0, full.indexOf(0));
-    }
-
-    public static String bytesToStr(byte[] bytes) {
-        return new String(bytes, UTF_8);
     }
 
     // TODO: replace by Address.toByteArray()
--- src/test/java/io/singularitynet/sdk/common/UtilsTest.java
+++ src/test/java/io/singularitynet/sdk/common/UtilsTest.java
@@ -1,6 +1,7 @@
 package io.singularitynet.sdk.common;
 
 import org.junit.*;
+import static org.junit.Assert.*;
 import org.junit.rules.ExpectedException;
 
 public class UtilsTest {
@@ -18,4 +19,34 @@ public class UtilsTest {
         });
     }
 
+    @Test
+    public void strToBytes32ChecksArgumentLength() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Passed string length exceeds 32 bytes");
+
+        Utils.strToBytes32("012345678901234567890123456789012");
+    }
+
+    @Test
+    public void strToBytes32AddsZeroPadding() {
+        byte[] bytes32 = Utils.strToBytes32("");
+
+        assertArrayEquals("Bytes32 from empty string", new byte[32], bytes32);
+    }
+
+    @Test
+    public void bytes32ToStrCheckArgumentNotTooLong() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Passed array length is not equal to 32 bytes");
+
+        Utils.bytes32ToStr(new byte[33]);
+    }
+
+    @Test
+    public void bytes32ToStrCheckArgumentNotTooShort() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Passed array length is not equal to 32 bytes");
+
+        Utils.bytes32ToStr(new byte[31]);
+    }
 }
