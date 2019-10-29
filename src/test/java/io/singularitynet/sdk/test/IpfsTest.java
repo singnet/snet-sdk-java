0c3effd0d08bf895dceba7dfaf2b1fcdea77c8f0
Vitaly Bogdanov <vsbogd@gmail.com>
2019-10-29 17:24:47 +0300

    Add URI API unit test

--- src/test/java/io/singularitynet/sdk/test/IpfsTest.java
+++ src/test/java/io/singularitynet/sdk/test/IpfsTest.java
@@ -3,9 +3,11 @@ package io.singularitynet.sdk.test;
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URI;
 import io.ipfs.api.*;
 import io.ipfs.multihash.*;
-import java.io.IOException;
 
 public class IpfsTest {
 
@@ -15,5 +17,12 @@ public class IpfsTest {
         Multihash filePointer = Multihash.fromBase58("QmNt1HZ5HWDTGusMuWQnRQCEeug7z55tSesCeKvjhATfTG");
         byte[] fileContents = ipfs.cat(filePointer);
         assertTrue("Non empty file from IPFS or exception is expected", fileContents.length > 0);
+    }
+
+    @Test
+    public void newIpfsUri() throws Exception {
+        URI uri = new URI("ipfs://Qma2KoWcf7f3c1m9nbr27LoPPHGonBBaTZeuxJ9L48CLS1");
+
+        assertEquals("URI authority", "Qma2KoWcf7f3c1m9nbr27LoPPHGonBBaTZeuxJ9L48CLS1", uri.getAuthority());
     }
 }
