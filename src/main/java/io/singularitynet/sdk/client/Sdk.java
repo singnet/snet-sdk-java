f6145293a2cbb1a04e8f401cabf9fb801bf72e67
Vitaly Bogdanov <vsbogd@gmail.com>
2019-10-30 20:16:45 +0300

    Add SDK shutdown method

--- src/main/java/io/singularitynet/sdk/client/Sdk.java
+++ src/main/java/io/singularitynet/sdk/client/Sdk.java
@@ -1,6 +1,7 @@
 package io.singularitynet.sdk.client;
 
 import org.web3j.tx.ReadonlyTransactionManager;
+import org.web3j.protocol.Web3j;
 import com.google.gson.Gson;
 import java.io.InputStreamReader;
 import com.google.common.base.Preconditions;
@@ -30,23 +31,25 @@ import io.singularitynet.sdk.ethereum.Address;
 public class Sdk {
 
     private final Configuration config;
+    private final Web3j web3j;
 
     public Sdk(Configuration config) {
         this.config = config;
+        this.web3j = config.getWeb3j();
     }
 
     public ServiceClient newServiceClient(String orgId, String serviceId,
             String endpointGroupName, PaymentStrategy paymentStrategy) {
 
         String networkId = Utils.wrapExceptions(() -> {
-            return config.getWeb3j().netVersion().send().getNetVersion();
+            return web3j.netVersion().send().getNetVersion();
         });
         ReadonlyTransactionManager transactionManager = new ReadonlyTransactionManager(
                 // FIXME: add unit test on prefix adding
-                config.getWeb3j(), config.getSigner().getAddress().toString());
+                web3j, config.getSigner().getAddress().toString());
 
         Address registryAddress = readContractAddress(networkId, "networks/Registry.json", "Registry");
-        Registry registry = Registry.load(registryAddress.toString(), config.getWeb3j(),
+        Registry registry = Registry.load(registryAddress.toString(), web3j,
                 transactionManager, config.getContractGasProvider());
         RegistryContract registryContract = new RegistryContract(registry);
         MetadataStorage metadataStorage = new IpfsMetadataStorage(config.getIpfs());
@@ -54,19 +57,23 @@ public class Sdk {
                 orgId, serviceId, registryContract, metadataStorage);
 
         Address mpeAddress = readContractAddress(networkId, "networks/MultiPartyEscrow.json", "MultiPartyEscrow");
-        MultiPartyEscrow mpe = MultiPartyEscrow.load(mpeAddress.toString(), config.getWeb3j(),
+        MultiPartyEscrow mpe = MultiPartyEscrow.load(mpeAddress.toString(), web3j,
                 transactionManager, config.getContractGasProvider());
         MultiPartyEscrowContract mpeContract = new MultiPartyEscrowContract(mpe);
 
         DaemonConnection connection = new FirstEndpointDaemonConnection(
                 endpointGroupName, metadataProvider);
         PaymentChannelStateService stateService = new PaymentChannelStateService(
-                connection, config.getWeb3j(), config.getSigner());
+                connection, web3j, config.getSigner());
         PaymentChannelProvider paymentChannelProvider =
             new AskDaemonFirstPaymentChannelProvider(mpeContract, stateService);
 
         return new BaseServiceClient(connection, metadataProvider,
                 paymentChannelProvider, paymentStrategy); 
+    }
+
+    public void shutdown() {
+        web3j.shutdown();
     }
 
     private static Address readContractAddress(String networkId, String networkJson, String contractName) {
