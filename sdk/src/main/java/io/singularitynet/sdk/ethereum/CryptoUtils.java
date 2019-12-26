ee257b6f50a9e0f78dab9f399513549f07d970bc
Vitaly Bogdanov <vsbogd@gmail.com>
2019-12-26 12:44:30 +0300

    Use web3j SignatureData as signature internal representation

--- sdk/src/main/java/io/singularitynet/sdk/ethereum/CryptoUtils.java
+++ sdk/src/main/java/io/singularitynet/sdk/ethereum/CryptoUtils.java
@@ -1,7 +1,5 @@
 package io.singularitynet.sdk.ethereum;
 
-import static io.singularitynet.sdk.common.Preconditions.checkState;
-import java.io.ByteArrayInputStream;
 import java.math.BigInteger;
 import org.web3j.crypto.Keys;
 import org.web3j.crypto.Sign;
@@ -16,23 +14,10 @@ public class CryptoUtils {
 
     public static Address getSignerAddress(byte[] message, Signature signature) {
         return Utils.wrapExceptions(() -> {
-            Sign.SignatureData signatureData = bytesToSignature(signature.getBytes());
+            Sign.SignatureData signatureData = signature.getSignatureData();
             BigInteger publicKey = Sign.signedPrefixedMessageToKey(Hash.sha3(message), signatureData);
             return new Address(Keys.getAddress(publicKey));
         });
     }
 
-    private static Sign.SignatureData bytesToSignature(byte[] signature) {
-        checkState(signature.length == 65, "Incorrect signature length: not equal to 65");
-        byte[] r = new byte[32];
-        byte[] s = new byte[32];
-        byte[] v = new byte[1];
-        return Utils.wrapExceptions(() -> {
-            ByteArrayInputStream bytes = new ByteArrayInputStream(signature);
-            bytes.read(r, 0, 32);
-            bytes.read(s, 0, 32);
-            bytes.read(v, 0, 1);
-            return new Sign.SignatureData(v[0], r, s);
-        });
-    }
 }
--- sdk/src/main/java/io/singularitynet/sdk/ethereum/PrivateKeyIdentity.java
+++ sdk/src/main/java/io/singularitynet/sdk/ethereum/PrivateKeyIdentity.java
@@ -1,12 +1,10 @@
 package io.singularitynet.sdk.ethereum;
 
 import java.math.BigInteger;
-import java.io.ByteArrayOutputStream;
 import org.web3j.crypto.ECKeyPair;
 import org.web3j.crypto.Sign;
 import org.web3j.crypto.Keys;
 import org.web3j.crypto.Hash;
-import static io.singularitynet.sdk.common.Preconditions.checkState;
 
 import io.singularitynet.sdk.common.Utils;
 
@@ -29,22 +27,12 @@ public class PrivateKeyIdentity implements Signer {
     @Override
     public Signature sign(byte[] message) {
         Sign.SignatureData signature = Sign.signPrefixedMessage(Hash.sha3(message), key);
-        return new Signature(signatureToBytes(signature));
+        return new Signature(signature);
     }
 
     @Override
     public Address getAddress() {
         return new Address(Keys.getAddress(key.getPublicKey()));
-    }
-
-    private static byte[] signatureToBytes(Sign.SignatureData signature) {
-        return Utils.wrapExceptions(() -> {
-            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-            bytes.write(signature.getR());
-            bytes.write(signature.getS());
-            bytes.write(signature.getV());
-            return bytes.toByteArray();
-        });
     }
 
 }
