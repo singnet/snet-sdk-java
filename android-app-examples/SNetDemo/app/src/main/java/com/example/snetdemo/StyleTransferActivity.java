5d86769dbbd44fe0d9cf617c8784bc0b8fae251d
Maxim Peterson <maxim.peterson@gmail.com>
2020-02-05 15:43:07 +0300

    change custom image show activity on android deafault

--- android-app-examples/SNetDemo/app/src/main/java/com/example/snetdemo/StyleTransferActivity.java
+++ android-app-examples/SNetDemo/app/src/main/java/com/example/snetdemo/StyleTransferActivity.java
@@ -11,7 +11,6 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
