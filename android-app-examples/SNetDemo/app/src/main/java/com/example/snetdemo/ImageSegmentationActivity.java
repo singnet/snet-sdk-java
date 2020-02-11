831074a6ced43bb3c2f7916917a12577a07fb33a
Maxim Peterson <maxim.peterson@gmail.com>
2020-02-11 11:22:06 +0300

    before merge

--- android-app-examples/SNetDemo/app/src/main/java/com/example/snetdemo/ImageSegmentationActivity.java
+++ android-app-examples/SNetDemo/app/src/main/java/com/example/snetdemo/ImageSegmentationActivity.java
@@ -10,7 +10,6 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
