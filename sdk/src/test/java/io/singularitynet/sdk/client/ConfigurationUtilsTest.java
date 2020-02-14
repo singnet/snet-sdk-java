2b00de5faee59f024ebac0656e79e59da1843128
Vitaly Bogdanov <vsbogd@gmail.com>
2020-02-14 20:55:26 +0300

    Simplify ConfigurationUtils.fromJson() unit test

--- sdk/src/test/java/io/singularitynet/sdk/client/ConfigurationUtilsTest.java
+++ sdk/src/test/java/io/singularitynet/sdk/client/ConfigurationUtilsTest.java
@@ -3,6 +3,7 @@ package io.singularitynet.sdk.client;
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.math.BigInteger;
 import java.net.URL;
 import java.net.MalformedURLException;
 import java.util.Optional;
@@ -13,101 +14,56 @@ import io.singularitynet.sdk.client.Configuration.IdentityType;
 
 public class ConfigurationUtilsTest {
 
-    private static String ENDPOINTS =
-            "\"ethereum_json_rpc_endpoint\": \"http://localhost:8545\"," +
-            "\"ipfs_endpoint\": \"http://localhost:5002\",";
-
-    private static String SIGNER =
-            "\"identity_type\": \"PRIVATE_KEY\"," +
-            "\"identity_private_key_hex\": \"010203\",";
-
-    private static String HEADER = "{" +
-            "\"ethereum_json_rpc_endpoint\": \"http://localhost:8545\"," +
-            "\"ipfs_endpoint\": \"http://localhost:5002\"," +
-            "\"identity_type\": \"PRIVATE_KEY\"," +
-            "\"identity_private_key_hex\": \"010203\"" +
-            "}";
-
     @Test
-    public void fromJsonConfigureEthereumJsonRpcEndpoint() throws MalformedURLException {
-        String ethereumJsonRpcEndpoint = "https://ropsten.infura.io";
-        String json = "{" + SIGNER +
+    public void fromJsonLoadAllProperties() throws MalformedURLException {
+        String ethereumJsonRpcEndpoint = "http://localhost:8545";
+        String ipfsEndpoint = "http://localhost:5002";
+        String privateKey = "01020304050607FAFBFCFDFEFF";
+        String registryAddress = "0x663422c6999Ff94933DBCb388623952CF2407F6f";
+        String mpeAddress = "0x8FB1dC8df86b388C7e00689d1eCb533A160B4D0C";
+        String json = "{" +
             "\"ethereum_json_rpc_endpoint\": \"" + ethereumJsonRpcEndpoint + "\"," +
-            "\"ipfs_endpoint\": \"http://localhost:5002\"" +
+            "\"ipfs_endpoint\": \"" + ipfsEndpoint + "\"," +
+            "\"identity_type\": \"PRIVATE_KEY\"," +
+            "\"identity_private_key_hex\": \"" + privateKey + "\"," +
+            "\"registry_address\": \"" + registryAddress + "\"," +
+            "\"multi_party_escrow_address\": \"" + mpeAddress + "\"," +
+            "\"gas_price\": \"1000000\"," +
+            "\"gas_limit\": \"2000000\"" +
             "}";
 
         Configuration config = ConfigurationUtils.fromJson(json);
 
         assertEquals("Ethereum JSON RPC endpoint", new URL(ethereumJsonRpcEndpoint), config.getEthereumJsonRpcEndpoint());
-    }
-
-    @Test
-    public void fromJsonConfigureIpfsEndpoint() throws MalformedURLException {
-        String ipfsEndpoint = "http://ipfs.singularitynet.io:80";
-        String json = "{" + SIGNER +
-            "\"ethereum_json_rpc_endpoint\": \"http://localhost:8545\"," +
-            "\"ipfs_endpoint\": \"" + ipfsEndpoint + "\"" +
-            "}";
-
-        Configuration config = ConfigurationUtils.fromJson(json);
-
-        assertEquals("IPFS URL", new URL(ipfsEndpoint), config.getIpfsEndpoint());
-    }
-
-    @Test
-    public void fromJsonConfigureIdentityPrivateKey() {
-        byte[] privateKey = Utils.base64ToBytes("1PeCDRD7vLjqiGoHl7A+yPuJIy8TdbNc1vxOyuPjxBM=");
-        String json = "{" + ENDPOINTS +
-            "\"identity_type\": \"PRIVATE_KEY\"," +
-            "\"identity_private_key_hex\": \"" + Utils.bytesToHex(privateKey) + "\"" +
-            "}";
-
-        Configuration config = ConfigurationUtils.fromJson(json);
-
+        assertEquals("IPFS endpoint", new URL(ipfsEndpoint), config.getIpfsEndpoint());
         assertEquals("Identity type", IdentityType.PRIVATE_KEY, config.getIdentityType());
-        assertArrayEquals("Identity private key", privateKey, config.getIdentityPrivateKey().get());
+        assertArrayEquals("Identity private key", Utils.hexToBytes(privateKey), config.getIdentityPrivateKey().get());
+        assertEquals("Registry address", new Address(registryAddress), config.getRegistryAddress().get());
+        assertEquals("MultiPartyEscrow address", new Address(mpeAddress), config.getMultiPartyEscrowAddress().get());
+        assertEquals("Gas price", BigInteger.valueOf(1000000), config.getGasPrice().get());
+        assertEquals("Gas limit", BigInteger.valueOf(2000000), config.getGasLimit().get());
     }
 
     @Test
-    public void fromJsonConfigureRegistryAddress() {
-        String registryAddress = "0x663422c6999Ff94933DBCb388623952CF2407F6f";
-        String json = "{" + ENDPOINTS + SIGNER +
-            "\"registry_address\": \"" + registryAddress + "\"" +
+    public void fromJsonLoadAllRequiredProperties() throws MalformedURLException {
+        String ethereumJsonRpcEndpoint = "http://localhost:8545";
+        String ipfsEndpoint = "http://localhost:5002";
+        String json = "{" +
+            "\"ethereum_json_rpc_endpoint\": \"" + ethereumJsonRpcEndpoint + "\"," +
+            "\"ipfs_endpoint\": \"" + ipfsEndpoint + "\"," +
+            "\"identity_type\": \"PRIVATE_KEY\"" +
             "}";
 
         Configuration config = ConfigurationUtils.fromJson(json);
 
-        assertEquals("Registry address", Optional.of(new Address(registryAddress)), config.getRegistryAddress());
-    }
-
-    @Test
-    public void fromJsonConfigureDefaultRegistryAddress() {
-        String json = HEADER;
-
-        Configuration config = ConfigurationUtils.fromJson(json);
-
-        assertEquals("Registry address", Optional.<Address>empty(), config.getRegistryAddress());
-    }
-
-    @Test
-    public void fromJsonConfigureMultiPartyEscrowAddress() {
-        String mpeAddress = "0x8FB1dC8df86b388C7e00689d1eCb533A160B4D0C";
-        String json = "{" + ENDPOINTS + SIGNER +
-            "\"multi_party_escrow_address\": \"" + mpeAddress + "\"" +
-            "}";
-
-        Configuration config = ConfigurationUtils.fromJson(json);
-
-        assertEquals("MultiPartyEscrow address", Optional.of(new Address(mpeAddress)), config.getMultiPartyEscrowAddress());
-    }
-
-    @Test
-    public void fromJsonConfigureDefaultMultiPartyEscrowAddress() {
-        String json = HEADER;
-
-        Configuration config = ConfigurationUtils.fromJson(json);
-
-        assertEquals("MultiPartyEscrow address", Optional.<Address>empty(), config.getMultiPartyEscrowAddress());
+        assertEquals("Ethereum JSON RPC endpoint", new URL(ethereumJsonRpcEndpoint), config.getEthereumJsonRpcEndpoint());
+        assertEquals("IPFS endpoint", new URL(ipfsEndpoint), config.getIpfsEndpoint());
+        assertEquals("Identity type", IdentityType.PRIVATE_KEY, config.getIdentityType());
+        assertEquals("Identity private key", Optional.empty(), config.getIdentityPrivateKey());
+        assertEquals("Registry address", Optional.empty(), config.getRegistryAddress());
+        assertEquals("MultiPartyEscrow address", Optional.empty(), config.getMultiPartyEscrowAddress());
+        assertEquals("Gas price", Optional.empty(), config.getGasPrice());
+        assertEquals("Gas limit", Optional.empty(), config.getGasLimit());
     }
 
 }
