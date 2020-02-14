7a2c370273ba792d2cc0e0e16250ac3ad6d21377
Vitaly Bogdanov <vsbogd@gmail.com>
2020-02-14 21:10:15 +0300

    Move constants into test header

--- sdk/src/test/java/io/singularitynet/sdk/client/ConfigurationUtilsTest.java
+++ sdk/src/test/java/io/singularitynet/sdk/client/ConfigurationUtilsTest.java
@@ -14,51 +14,53 @@ import io.singularitynet.sdk.client.Configuration.IdentityType;
 
 public class ConfigurationUtilsTest {
 
+    private final static String ethereumJsonRpcEndpoint = "http://localhost:8545";
+    private final static String ipfsEndpoint = "http://localhost:5002";
+    private final static String identityType = "PRIVATE_KEY";
+    private final static String privateKey = "01020304050607FAFBFCFDFEFF";
+    private final static String registryAddress = "0x663422c6999Ff94933DBCb388623952CF2407F6f";
+    private final static String mpeAddress = "0x8FB1dC8df86b388C7e00689d1eCb533A160B4D0C";
+    private final static String gasPrice = "1000000";
+    private final static String gasLimit = "2000000";
+
     @Test
     public void fromJsonLoadAllProperties() throws MalformedURLException {
-        String ethereumJsonRpcEndpoint = "http://localhost:8545";
-        String ipfsEndpoint = "http://localhost:5002";
-        String privateKey = "01020304050607FAFBFCFDFEFF";
-        String registryAddress = "0x663422c6999Ff94933DBCb388623952CF2407F6f";
-        String mpeAddress = "0x8FB1dC8df86b388C7e00689d1eCb533A160B4D0C";
         String json = "{" +
             "\"ethereum_json_rpc_endpoint\": \"" + ethereumJsonRpcEndpoint + "\"," +
             "\"ipfs_endpoint\": \"" + ipfsEndpoint + "\"," +
-            "\"identity_type\": \"PRIVATE_KEY\"," +
+            "\"identity_type\": \"" + identityType + "\"," +
             "\"identity_private_key_hex\": \"" + privateKey + "\"," +
             "\"registry_address\": \"" + registryAddress + "\"," +
             "\"multi_party_escrow_address\": \"" + mpeAddress + "\"," +
-            "\"gas_price\": \"1000000\"," +
-            "\"gas_limit\": \"2000000\"" +
+            "\"gas_price\": \"" + gasPrice + "\"," +
+            "\"gas_limit\": \"" + gasLimit + "\"" +
             "}";
 
         Configuration config = ConfigurationUtils.fromJson(json);
 
         assertEquals("Ethereum JSON RPC endpoint", new URL(ethereumJsonRpcEndpoint), config.getEthereumJsonRpcEndpoint());
         assertEquals("IPFS endpoint", new URL(ipfsEndpoint), config.getIpfsEndpoint());
-        assertEquals("Identity type", IdentityType.PRIVATE_KEY, config.getIdentityType());
+        assertEquals("Identity type", Enum.valueOf(Configuration.IdentityType.class, identityType), config.getIdentityType());
         assertArrayEquals("Identity private key", Utils.hexToBytes(privateKey), config.getIdentityPrivateKey().get());
         assertEquals("Registry address", new Address(registryAddress), config.getRegistryAddress().get());
         assertEquals("MultiPartyEscrow address", new Address(mpeAddress), config.getMultiPartyEscrowAddress().get());
-        assertEquals("Gas price", BigInteger.valueOf(1000000), config.getGasPrice().get());
-        assertEquals("Gas limit", BigInteger.valueOf(2000000), config.getGasLimit().get());
+        assertEquals("Gas price", new BigInteger(gasPrice), config.getGasPrice().get());
+        assertEquals("Gas limit", new BigInteger(gasLimit), config.getGasLimit().get());
     }
 
     @Test
     public void fromJsonLoadAllRequiredProperties() throws MalformedURLException {
-        String ethereumJsonRpcEndpoint = "http://localhost:8545";
-        String ipfsEndpoint = "http://localhost:5002";
         String json = "{" +
             "\"ethereum_json_rpc_endpoint\": \"" + ethereumJsonRpcEndpoint + "\"," +
             "\"ipfs_endpoint\": \"" + ipfsEndpoint + "\"," +
-            "\"identity_type\": \"PRIVATE_KEY\"" +
+            "\"identity_type\": \"" + identityType + "\"" +
             "}";
 
         Configuration config = ConfigurationUtils.fromJson(json);
 
         assertEquals("Ethereum JSON RPC endpoint", new URL(ethereumJsonRpcEndpoint), config.getEthereumJsonRpcEndpoint());
         assertEquals("IPFS endpoint", new URL(ipfsEndpoint), config.getIpfsEndpoint());
-        assertEquals("Identity type", IdentityType.PRIVATE_KEY, config.getIdentityType());
+        assertEquals("Identity type", Enum.valueOf(Configuration.IdentityType.class, identityType), config.getIdentityType());
         assertEquals("Identity private key", Optional.empty(), config.getIdentityPrivateKey());
         assertEquals("Registry address", Optional.empty(), config.getRegistryAddress());
         assertEquals("MultiPartyEscrow address", Optional.empty(), config.getMultiPartyEscrowAddress());
